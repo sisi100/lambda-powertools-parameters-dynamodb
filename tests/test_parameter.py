7186f60422f9e74ec5c1074b3d21dc6c6222bdf1
Shun Isii <sisii00.swork@gmail.com>
2021-11-10 11:21:43 +0900

    更新

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -6,11 +6,15 @@ from moto import mock_dynamodb2
 TABLE_NAME = "hogehoge"
 DUMMY_ITEMS = 5  # ダミーデータを積む数
 
+# ======================== テスト用のテーブルとダミーデータ↓
+
 
 @pytest.fixture()
-def mock_dynamodb_table(monkeypatch):
+def initialize(monkeypatch):
+
     mock_dynamodb2().start()
 
+    # テスト用のテーブルを作成する
     dynamodb = boto3.resource("dynamodb")
     dynamodb.create_table(
         TableName=TABLE_NAME,
@@ -22,24 +26,41 @@ def mock_dynamodb_table(monkeypatch):
         ],
         ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
     )
+    table = dynamodb.Table(TABLE_NAME)
 
-    yield dynamodb.Table(TABLE_NAME)
+    # ダミーデータをテーブルに積む
+    for item in [dict(hoge_pk=i, hoge_value=f"hoge_{i}") for i in range(DUMMY_ITEMS)]:
+        table.put_item(Item=item)
+
+    yield
 
     mock_dynamodb2().stop()
 
 
-def put_dummy_data(table):
-    """ダミーデータをテーブルに積む"""
-    for item in [dict(hoge_pk=i, hoge_value=f"hoge_{i}") for i in range(DUMMY_ITEMS)]:
-        table.put_item(Item=item)
+# ======================== ↓動作確認用（LambdaPowertoolsを使った場合）
 
 
-def test_dynamodb_parameters(mock_dynamodb_table):
-    put_dummy_data(mock_dynamodb_table)
+def test_dynamodb_parameters(initialize):
 
     dynamodb_provider = parameters.DynamoDBProvider(table_name=TABLE_NAME, key_attr="hoge_pk", value_attr="hoge_value")
 
     for i in range(DUMMY_ITEMS):
         value = dynamodb_provider.get(i)
+
+        print(value)
+        assert value == f"hoge_{i}"
+
+
+# ======================== ↓動作確認用（Boto3を使った場合）
+
+
+def test_boto3(initialize):
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(TABLE_NAME)
+
+    for i in range(DUMMY_ITEMS):
+        value = table.get_item(Key={"hoge_pk": i})["Item"]["hoge_value"]
+
         print(value)
         assert value == f"hoge_{i}"
